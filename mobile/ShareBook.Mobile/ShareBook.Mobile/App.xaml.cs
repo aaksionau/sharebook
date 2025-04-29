12eee99a5ea9828ca0b310cbbf0b1fcd6726c910
Aliaksei Aksionau <alexei.aksenov@gmail.com>
2025-04-28 22:18:39 -0500

    Add initial mobile app

---
+++ mobile/ShareBook.Mobile/ShareBook.Mobile/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ShareBook.Mobile;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ mobile/ShareBook.Mobile/ShareBook.Mobile/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ShareBook.Mobile;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
